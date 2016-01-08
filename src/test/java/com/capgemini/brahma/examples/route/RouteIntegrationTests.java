7058ef91da486aa58754800e7e76cc8ac15847a1
Andrew Harmel-Law <andrew.harmel-law@capgemini.com>
2016-01-08 17:06:44 +0000

    Tidy up and bit of example route (not used) and a todo to add a test which hits the SOAP service validly

--- src/test/java/com/capgemini/brahma/examples/route/RouteIntegrationTests.java
+++ src/test/java/com/capgemini/brahma/examples/route/RouteIntegrationTests.java
@@ -3,7 +3,6 @@ package com.capgemini.brahma.examples.route;
 import com.capgemini.brahma.Application;
 import com.jayway.restassured.RestAssured;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -29,4 +28,6 @@ public class RouteIntegrationTests {
         when().post("/CamelContext/RouterPort").then().body(equalTo("<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\"><soap:Body><soap:Fault><faultcode>soap:Client</faultcode><faultstring>Error reading XMLStreamReader: Unexpected EOF in prolog\n" +
                 " at [row,col {unknown-source}]: [1,0]</faultstring></soap:Fault></soap:Body></soap:Envelope>"));
     }
+
+    // TODO: add tests which send in valid requests and get a valid response back (even if its canned).
 }
