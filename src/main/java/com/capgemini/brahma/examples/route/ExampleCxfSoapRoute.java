b2484856457166d9ef77192f5227294b2a8afd93
Andrew Harmel-Law <andrew.harmel-law@capgemini.com>
2016-01-08 17:06:44 +0000

    Tidy up and bit of example route (not used) and a todo to add a test which hits the SOAP service validly

--- src/main/java/com/capgemini/brahma/examples/route/ExampleCxfSoapRoute.java
+++ src/main/java/com/capgemini/brahma/examples/route/ExampleCxfSoapRoute.java
@@ -1,7 +1,7 @@
 package com.capgemini.brahma.examples.route;
 
 import org.apache.camel.Exchange;
-import org.apache.camel.LoggingLevel;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -11,23 +11,18 @@ public class ExampleCxfSoapRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
+        // TODO: Pull this bit into an abstract superclass - a CxfSoapRouteBuilder class
         from("cxf:bean:routerEndpoint")
                 .to("cxf:bean:serviceEndpoint");
 
-//        from("cxf:bean:serviceEndpoint").
+        from("cxf:bean:serviceEndpoint")
+                .to("log:info")
+                .process(new Processor() {
+                    @SuppressWarnings("unchecked")
+                    public void process(final Exchange exchange) throws Exception {
+                        exchange.getOut().setBody("THIS IS A CANNED RESPONSE PAYLOAD");
+                    }
+                });
 
-//        from("cxf:bean:subscriptionSOAPService")
-//                .routeId(ExampleCxfSoapRoute.class.getSimpleName())
-//                .onCompletion()
-//                .log(LoggingLevel.INFO, "!!!!!!!!!!!!!!!!!" + ExampleCxfSoapRoute.class.getSimpleName() + " completed.")
-//                .removeHeaders("*", Exchange.HTTP_RESPONSE_CODE)
-//                .id("routeEnd")
-//                .end()
-//                .to("log:foo");
-////                .choice()
-////                    .when(header("soapOperationName").isEqualTo("GetStatus"))
-////                        .to("direct:getSubscriptionStatus")
-////                    .end()
-////                .end();
     }
 }
