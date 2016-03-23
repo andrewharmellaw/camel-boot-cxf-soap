ce0b54b63a0a2b8645cdadc7ca09ff6c8d2a6617
Nikhil Vibhav <nikhil92ind@gmail.com>
2016-03-23 09:34:57 +0530

    added missing import for @Value annotation

--- src/integrationtest/java/com/capgemini/brahma/examples/route/RouteIntegrationTests.java
+++ src/integrationtest/java/com/capgemini/brahma/examples/route/RouteIntegrationTests.java
@@ -5,6 +5,7 @@ import com.jayway.restassured.RestAssured;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
