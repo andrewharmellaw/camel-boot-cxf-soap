bcbec570aea4cf16b118518bf7158f04f012563e
gthiyaga <gayathri.thiyagarajan@capgemini.com>
2016-04-08 14:52:37 +0100

    To be able to run as stand alone jar

--- src/main/java/com/capgemini/brahma/Application.java
+++ src/main/java/com/capgemini/brahma/Application.java
@@ -4,6 +4,7 @@ import com.capgemini.camel.metrics.publisher.configuration.MetricsServletConfigu
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.camel.spring.boot.FatJarRouter;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
@@ -13,4 +14,7 @@ public class Application extends FatJarRouter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
